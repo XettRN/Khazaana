11180de6100c42f3ad51d52cd7144ebad5f0c255
Varshita Patakottu <varshita13@gmail.com>
2021-10-11 23:49:45 -0400

    implemented google auth

--- app/src/main/java/com/example/khazaana/LoadScreen.java
+++ app/src/main/java/com/example/khazaana/LoadScreen.java
@@ -5,14 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoadScreen extends AppCompatActivity {
 
     private TextView title;
     private Button signIn;
+    private SignInButton googleSignIn;
+
+    public static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,22 @@ public class LoadScreen extends AppCompatActivity {
         signIn = findViewById((R.id.sign_in));
         Typeface mont = Typeface.createFromAsset(getAssets(), "fonts/Montserrat-Light.otf");
         title.setTypeface(mont);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        googleSignIn = findViewById(R.id.google);
+        googleSignIn.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.google:
+                        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                        startActivityForResult(signInIntent, RC_SIGN_IN);
+                        break;
+                    // ...
+                }
+            }
+        });
 
         signIn.setOnClickListener( new View.OnClickListener() {
             public void onClick(View v) {
@@ -30,4 +58,43 @@ public class LoadScreen extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this); // if user is already signed in
+        if (account != null) {
+            // transition to home page screen
+        } else {
+            // display load screen
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI
+            //updateUI(account); transition to home page
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+//            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+//
+//            updateUI(null); // stay on load screen
+        }
+    }
+
 }
