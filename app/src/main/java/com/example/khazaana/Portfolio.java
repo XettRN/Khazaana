26b578a608dcb2d83ad8227db0b55b270bc41455
Kyle <ttex101@gmail.com>
2021-10-19 20:57:16 -0400

    Create Pie Chart

--- app/src/main/java/com/example/khazaana/Portfolio.java
+++ app/src/main/java/com/example/khazaana/Portfolio.java
@@ -4,11 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class Portfolio extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfolio);
+
+        PieChart pieChart = findViewById(R.id.pieChart);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setHoleRadius(0f);
+        pieChart.setTransparentCircleRadius(0f);
+        pieChart.getLegend().setEnabled(false);
+        pieChart.setData(getPieData());
+
+        //needed to update chart later on
+        //pieChart.invalidate();
     }
+
+    private PieData getPieData() {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(40f, "Stocks"));
+        entries.add(new PieEntry(60f, "Crypto"));
+
+        PieDataSet pieDataSet = new PieDataSet(entries , "");
+        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        pieDataSet.setDrawValues(false);
+
+        return new PieData(pieDataSet);
+    }
+
 }
