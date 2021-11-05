4a0a91308c4324c4bde8dce3d505326e7f6b23cd
Varshita Patakottu <varshita13@gmail.com>
2021-11-05 01:24:16 -0400

    added transitions between screens

--- app/src/main/java/com/example/khazaana/ClientAccountSettings.java
+++ app/src/main/java/com/example/khazaana/ClientAccountSettings.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -21,6 +22,36 @@ public class ClientAccountSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.client_account_settings);
+        Button profile = findViewById(R.id.profile);
+        profile.setOnClickListener(this::goClientProfile);
 
+        Button fees = findViewById(R.id.fees);
+        fees.setOnClickListener(this::goClientFees);
+
+        Button risk = findViewById(R.id.risk_profiling);
+        risk.setOnClickListener(this::goRiskProfiling);
+
+        Button settings = findViewById(R.id.settings);
+        settings.setOnClickListener(this::goClientSettings);
+    }
+
+    public void goClientProfile(View view) {
+        Intent intent = new Intent(this, ClientProfile.class);
+        startActivity(intent);
+    }
+
+    public void goClientFees(View view) {
+        Intent intent = new Intent(this, ClientFees.class);
+        startActivity(intent);
+    }
+
+    public void goRiskProfiling(View view) {
+        Intent intent = new Intent(this, ClientRiskProfilingMain.class);
+        startActivity(intent);
+    }
+
+    public void goClientSettings(View view) {
+        Intent intent = new Intent(this, ClientSettings.class);
+        startActivity(intent);
     }
 }
