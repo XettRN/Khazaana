dd1168fda9e36d56963f5d1f59a7060b337e727d
tejas bhuwania <tbhuwania3@gatech.edu>
2021-11-14 16:38:15 -0500

    add risk profiling

---
+++ app/src/main/java/com/example/khazaana/RiskProfiling_5.java
@@ -0,0 +1,13 @@
+package com.example.khazaana;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RiskProfiling_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.risk_profiling_5);
+    }
+}
---
+++ app/src/main/java/com/example/khazaana/RiskProfiling_9.java
@@ -0,0 +1,13 @@
+package com.example.khazaana;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RiskProfiling_9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.risk_profiling_9);
+    }
+}
