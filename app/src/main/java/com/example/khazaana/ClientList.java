851f5187b54e9396a45df0d80d45c3594402ae61
Kyle <ttex101@gmail.com>
2021-11-15 16:47:21 -0500

    Add toolbar manual entry test

--- app/src/main/java/com/example/khazaana/ClientList.java
+++ app/src/main/java/com/example/khazaana/ClientList.java
@@ -1,10 +1,14 @@
 package com.example.khazaana;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class ClientList extends AppCompatActivity {
     @Override
@@ -14,10 +18,25 @@ public class ClientList extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int i = item.getItemId();
+        if (i == R.id.toolbar_add) {
+            goAddData(item);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_list);
         Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
+    }
+
+    public void goAddData(MenuItem item) {
+        Intent intent = new Intent(this, AddData.class);
+        startActivity(intent);
     }
 }
