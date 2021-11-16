ceb5705e51d02c3de9f57b3908e48383fd928f89
tejas bhuwania <tbhuwania3@gatech.edu>
2021-11-16 04:11:37 -0500

    add interactions

--- app/src/main/java/com/example/khazaana/RiskProfiling_6.java
+++ app/src/main/java/com/example/khazaana/RiskProfiling_6.java
@@ -1,7 +1,11 @@
 package com.example.khazaana;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RiskProfiling_6 extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class RiskProfiling_6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.risk_profiling_6);
+
+        Button nextPage = findViewById(R.id.next8);
+        nextPage.setOnClickListener(this::nextPage);
+
+        Button previousPage = findViewById(R.id.button6);
+        previousPage.setOnClickListener(this::previousPage);
+    }
+
+    public void nextPage(View view) {
+        Intent intent = new Intent(this, RiskProfiling_7.class);
+        startActivity(intent);
+    }
+
+    public void previousPage(View view) {
+        Intent intent = new Intent(this, RiskProfiling_5.class);
+        startActivity(intent);
     }
 }
