72a7c917d0bfb3c8b70f13f35e14ffffa4c8b628
tejas bhuwania <tbhuwania3@gatech.edu>
2021-11-20 22:23:20 -0500

    update p2

--- app/src/main/java/com/example/khazaana/RiskProfiling_1.java
+++ app/src/main/java/com/example/khazaana/RiskProfiling_1.java
@@ -6,8 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.util.Log;
+
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipDrawable;
+import com.google.android.material.chip.ChipGroup;
 
 public class RiskProfiling_1 extends AppCompatActivity {
+    private final String TAG = "Chips Example";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +23,16 @@ public class RiskProfiling_1 extends AppCompatActivity {
         Button nextPage = findViewById(R.id.next2);
         nextPage.setOnClickListener(this::nextPage);
     }
+
+    ChipGroup choiceChipGroup = findViewById(R.id.choice_chip_group);
+        choiceChipGroup.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener()
+
+    {
+        @Override
+        public void onCheckedChanged (ChipGroup chipGroup,@IdRes int i){
+        Log.i(TAG, i + "");
+    }
+    });
 
     public void nextPage(View view) {
         Intent intent = new Intent(this, RiskProfiling_2.class);
