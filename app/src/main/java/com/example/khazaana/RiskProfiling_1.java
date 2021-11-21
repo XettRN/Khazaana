e4dc7d5dc3dee8173323d0189955256a70c4b006
rmarakana3 <rmarakana3@gatech.edu>
2021-11-21 17:26:31 -0500

    [Modidying the risk profiling screen]

--- app/src/main/java/com/example/khazaana/RiskProfiling_1.java
+++ app/src/main/java/com/example/khazaana/RiskProfiling_1.java
@@ -1,5 +1,6 @@
 package com.example.khazaana;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,7 +15,6 @@ import com.google.android.material.chip.ChipGroup;
 
 public class RiskProfiling_1 extends AppCompatActivity {
     private final String TAG = "Chips Example";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,17 +22,14 @@ public class RiskProfiling_1 extends AppCompatActivity {
 
         Button nextPage = findViewById(R.id.next2);
         nextPage.setOnClickListener(this::nextPage);
+        ChipGroup choiceChipGroup = findViewById(R.id.choice_chip_group);
+        choiceChipGroup.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(ChipGroup chipGroup, @IdRes int i) {
+                Log.i(TAG, i + "");
+            }
+        });
     }
-
-    ChipGroup choiceChipGroup = findViewById(R.id.choice_chip_group);
-        choiceChipGroup.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener()
-
-    {
-        @Override
-        public void onCheckedChanged (ChipGroup chipGroup,@IdRes int i){
-        Log.i(TAG, i + "");
-    }
-    });
 
     public void nextPage(View view) {
         Intent intent = new Intent(this, RiskProfiling_2.class);
