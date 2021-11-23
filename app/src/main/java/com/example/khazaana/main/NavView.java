7c457f9f05ecf6d631ad17f6493f4a491a824d28
Kyle <ttex101@gmail.com>
2021-11-23 13:34:58 -0500

    Start fragment migration

---
+++ app/src/main/java/com/example/khazaana/main/NavView.java
@@ -0,0 +1,16 @@
+package com.example.khazaana.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.khazaana.R;
+
+public class NavView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_view);
+    }
+}
