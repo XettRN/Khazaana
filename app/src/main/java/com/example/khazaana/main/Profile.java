3b0a75a3572416270a3ff952d73b16640069dc1e
tejas bhuwania <tbhuwania3@gatech.edu>
2021-12-06 20:22:19 -0500

    nav between Setting and IFA profile

--- app/src/main/java/com/example/khazaana/main/Profile.java
+++ app/src/main/java/com/example/khazaana/main/Profile.java
@@ -2,11 +2,16 @@ package com.example.khazaana.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.khazaana.R;
 
@@ -16,5 +21,19 @@ public class Profile extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ifa_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button previousPage = view.findViewById(R.id.back_ifa_profile);
+        previousPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavDirections action = ProfileDirections.actionProfileFragToIFAAccountSettings();
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
     }
 }
